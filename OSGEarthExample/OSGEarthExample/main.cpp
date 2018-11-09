e9233f6ac6e04cd792e9656c2d50be4747c97c72
summericeyl <summericeyl@gmail.com>
2018-11-09 19:52:36 +0800

    生成一个模板

---
+++ OSGEarthExample/OSGEarthExample/main.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <osgEarth/MapNode>
+
+#include <osgEarthUtil/EarthManipulator>
+#include <osgEarthUtil/ExampleResources>
+
+#include <osgEarthAnnotation/ImageOverlay>
+#include <osgEarthAnnotation/CircleNode>
+#include <osgEarthAnnotation/RectangleNode>
+#include <osgEarthAnnotation/EllipseNode>
+#include <osgEarthAnnotation/PlaceNode>
+#include <osgEarthAnnotation/LabelNode>
+#include <osgEarthAnnotation/LocalGeometryNode>
+#include <osgEarthAnnotation/FeatureNode>
+
+#include <osgEarthAnnotation/AnnotationEditing>
+#include <osgEarthAnnotation/ImageOverlayEditor>
+
+#include <osgEarthSymbology/GeometryFactory>
+
+#include <osgViewer/Viewer>
+
+int main(int argc, char** argv)
+{
+
+
+	return 0;
+}
